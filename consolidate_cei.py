9571dafb14ec32f5afe6157383aecbc1dc12adfb
danilo.pereira <danilo.pereira@radixeng.com.br>
2020-03-25 21:04:34 -0300

    adiciona mensagem de sucesso ao script de consolidacao

--- consolidate_cei.py
+++ consolidate_cei.py
@@ -38,6 +38,7 @@ def consolidate_cei_extracts(base_folder = 'extratos_cei', save_to_file = False)
         save_data = transactions.copy()
         save_data.index = save_data.index.dt.date
         save_data.to_excel('consolidado_cei.xlsx')
+        print('Dados salvos na planilha consolidado_cei.xlsx')
 
     return transactions
 
